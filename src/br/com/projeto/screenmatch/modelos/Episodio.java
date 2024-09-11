07ad567be2d8514d6171fe81bbd52ffeecb720d0
Nathalia Serra <nathaliaserrap@gmail.com>
2024-09-11 19:13:39 -0300

    Especializando o método de avaliação em estrelas para o episodio

--- src/br/com/projeto/screenmatch/modelos/Episodio.java
+++ src/br/com/projeto/screenmatch/modelos/Episodio.java
@@ -1,9 +1,20 @@
 package br.com.projeto.screenmatch.modelos;
 
-public class Episodio {
+import br.com.projeto.screenmatch.calculos.ClassificacaoEstrela;
+
+public class Episodio implements ClassificacaoEstrela {
     private int numeroDeEpisodios;
     private String nome;
     private Serie serie;
+    private int totalDeVisulizacoes;
+
+    public int getTotalDeVisulizacoes() {
+        return totalDeVisulizacoes;
+    }
+
+    public void setTotalDeVisulizacoes(int totalDeVisulizacoes) {
+        this.totalDeVisulizacoes = totalDeVisulizacoes;
+    }
 
     public int getNumeroDeEpisodios() {
         return numeroDeEpisodios;
@@ -27,5 +38,14 @@ public class Episodio {
 
     public void setSerie(Serie serie) {
         this.serie = serie;
+    }
+
+    @Override
+    public int getClassificacao() {
+        if (totalDeVisulizacoes > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
     }
 }
