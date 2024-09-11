cf6c36f82cfa2d590180e13b8e4c9c5e6083b4be
Nathalia Serra <nathaliaserrap@gmail.com>
2024-09-11 17:39:26 -0300

    Atribuindo valores ao objeto usando o set e imprimindo eles usando o get

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,14 @@
+import br.com.projeto.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
-        filme1.nome = "O Poderoso Chefão";
-        filme1.anoDeLancamento = 1972;
-        filme1.duracaoEmMinutos = 180;
+        filme1.setNome("O Poderoso Chefão");
+        filme1.setAnoDeLancamento(1972);
+        filme1.setDuracaoEmMinutos(180);
+        System.out.println(filme1.getNome());
+        System.out.println(filme1.getAnoDeLancamento());
+        System.out.println(filme1.getDuracaoEmMinutos());
 
         filme1.exibeFichaTecnica();
         filme1.avalia(8);
