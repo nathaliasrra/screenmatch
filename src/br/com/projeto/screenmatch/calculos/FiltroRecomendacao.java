610a88bcce04e12b4e24baf226df244c20c1f77c
Nathalia Serra <nathaliaserrap@gmail.com>
2024-09-11 19:18:54 -0300

    Criando método para retornar filtrar as recomendações

---
+++ src/br/com/projeto/screenmatch/calculos/FiltroRecomendacao.java
@@ -0,0 +1,16 @@
+package br.com.projeto.screenmatch.calculos;
+
+import br.com.projeto.screenmatch.modelos.Titulo;
+
+public class FiltroRecomendacao {
+
+    public void filtra(ClassificacaoEstrela classificavel) {
+        if(classificavel.getClassificacao() >= 4) {
+            System.out.println("Está entre os preferidos do momento");
+        } else if (classificavel.getClassificacao() >= 2){
+            System.out.println("Muito bem avaliado no momento");
+        } else {
+            System.out.println("Coloque na sua lista para assistir depois");
+        }
+    }
+}
