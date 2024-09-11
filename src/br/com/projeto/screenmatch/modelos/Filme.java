c44630d3d0ce00cb496dc63ac152bc5ee2e5b95e
Nathalia Serra <nathaliaserrap@gmail.com>
2024-09-11 17:35:14 -0300

    Modificando os atributos para privado e criando métodos para eles

--- src/br/com/projeto/screenmatch/modelos/Filme.java
+++ src/br/com/projeto/screenmatch/modelos/Filme.java
@@ -1,13 +1,31 @@
+package br.com.projeto.screenmatch.modelos;
+
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+    private int duracaoEmMinutos;
 
     int getTotalDeAvaliacoes() {
         return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
     }
 
     void exibeFichaTecnica() {
