8e1146a3eed71394322ee8561c9e973f3262bcb1
Nathalia Serra <nathaliaserrap@gmail.com>
2024-09-11 18:46:54 -0300

    Criando atributos e métodos para a classe episódio

---
+++ src/br/com/projeto/screenmatch/modelos/Episodio.java
@@ -0,0 +1,31 @@
+package br.com.projeto.screenmatch.modelos;
+
+public class Episodio {
+    private int numeroDeEpisodios;
+    private String nome;
+    private Serie serie;
+
+    public int getNumeroDeEpisodios() {
+        return numeroDeEpisodios;
+    }
+
+    public void setNumeroDeEpisodios(int numeroDeEpisodios) {
+        this.numeroDeEpisodios = numeroDeEpisodios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+}
