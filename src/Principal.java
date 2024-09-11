8479b8df98235d5d359e4b56965295edac6f04f1
Nathalia Serra <nathaliaserrap@gmail.com>
2024-09-11 16:37:56 -0300

    Aribuindo valores ao objeto e imprimindo no terminal

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,12 @@
 public class Principal {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
+        filme1.nome = "O Poderoso Chefão";
+        filme1.anoDeLancamento = 1972;
+        filme1.duracaoEmMinutos = 180;
+        System.out.println(filme1.nome);
+        System.out.println(filme1.anoDeLancamento);
+        System.out.println(filme1.duracaoEmMinutos);
 
     }
 }
